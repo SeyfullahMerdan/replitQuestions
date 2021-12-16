bf783f7bf0addcf57c1dde3f16ee7d5d9e82a63d
seyfullah merdan <merdanseyfullah33@gmail.com>
2021-12-17 00:44:16 +0100

    varargs

---
+++ src/Varargs/q1.java
@@ -0,0 +1,54 @@
+package Varargs;
+
+public class q1 {
+
+	/* Write a return method that accepts more
+	than one integer as parameter and prints the
+	sum of integers 
+	Method name=sum if you call method like that 
+	 sum(1,2,3) output =6 
+	 sum(1,2,3,4,5) output =15 
+	 sum(1,2) output=3	
+	*/
+	
+	public static void main(String[] args) {
+		
+		int nm1=1;
+		int nm2=2;
+		
+		sum (nm1,nm2);
+		sum (1,2,3,4,5);
+		sum (1,2,3);
+		
+		
+	}
+	
+	
+	
+	private static int sum (int... numbers) {
+		
+
+		int plus=0;
+		
+		for (int i : numbers) {
+			plus+=i;
+		}
+		
+		System.out.println(plus);
+		return plus;
+		
+		
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
